b95cbad1d56a54d67888bea6fcee66c71266ca10
Nicole Chan <nictscnotes@gmail.com>
2026-02-03 07:08:13 +1100

    Add checkout page

---
+++ pages/checkoutPage.ts
@@ -0,0 +1,34 @@
+import { Page, Locator } from '@playwright/test';
+import { BasePage } from './basePage';
+
+export class CheckoutPage extends BasePage {
+  // Declare what exists on the Checkout page
+  readonly finishButton: Locator;
+  readonly paymentInfoLabel: Locator;
+  readonly paymentInfoValue: Locator;
+  readonly shippingInfoLabel: Locator;
+  readonly shippingInfoValue: Locator;
+  readonly totalInfoValue: Locator;
+  readonly subTotalLabel: Locator;
+  readonly taxLabel: Locator;
+  readonly totalLabel: Locator;
+
+
+  constructor(page: Page) {
+    // Set up the Checkout page
+    super(page);
+
+    // Create unique locators on the Checkout page
+    this.finishButton = page.locator('[data-test="continue"]');
+    this.paymentInfoLabel = page.locator('[data-test="payment-info-value"]');
+    this.paymentInfoValue = page.locator('[data-test="shipping-info-label"]');
+    this.shippingInfoLabel = page.locator('[data-test="shipping-info-value"]');
+    this.shippingInfoValue = page.locator('[data-test="shipping-info-value"]');
+    this.totalInfoValue = page.locator('[data-test="total-info-label"]');
+    this.subTotalLabel = page.locator('[data-test="subtotal-label"');
+    this.taxLabel = page.locator('[data-test="tax-label"]');
+    this.totalLabel = page.locator('[data-test="total-label"]');
+  }
+
+  
+}
